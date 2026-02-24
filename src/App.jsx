2851a2557f58734c5c743eaa81014feae1856675
Nova06244 <nova06244@gmail.com>
2026-02-24 21:02:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React { useMemo, useState, useEffect, useRef } from "react";
+import react { useMemo, useState, useEffect, useRef } from "react";
 
 const INITIAL_ASSETS = [
   { id: 1, name: "SOLANA", ticker: "SOL", objective: "X10", color: "#14F195" },
